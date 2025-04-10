bc3cc00708733000a8cf74c5975fec1039bd2f01
Amanda Kube Jotte <akube@uchicago.edu>
2025-04-10 17:57:41 -0500

    Update test_classes.py

--- test_classes.py
+++ test_classes.py
@@ -14,18 +14,43 @@ try:
 except ImportError:
     pass
 
-class TestXYZ(unittest.TestCase):
+class TestShows(unittest.TestCase):
     def setUp(self):
+        self.test_show1 = Show("CBS", "Survivor", "21-5", "Alliances shift and tempers flare as trust issues erupt during a high-stakes immunity challenge.", "Reality", 1900, 2000, False)
+        self.test_show2 = Show("NBC", "The Office", "4-13", "Michael and Jan invite Jim, Pam, Andy, and Angela over for an incredibly awkward dinner.", "Comedy", 2000, 2100, False)
+        self.guide1 = Guide([self.test_show1, self.test_show2])
 
+        # You may add additional fixtures for your tests here
+        
 
-# author: wltrimbl
+# author: Instructors
     @weight(1)
     @number("0.0")
     def test_exist_pa4(self):
         '''Test that pa4.py exists'''
         self.assertTrue(exists("pa4.py"), "pa4.py does not exist!")
 
+# author: Instructors
+    @weight(1)
+    @number("0.1")
+    def test_show_runs(self):
+        '''Test Show constructor runs without error with valid input'''
+        def construct():
+            return Show("PBS", "Antiques Roadshow", "24-2", "A rare Magic: The Gathering card collection stuns appraisers with a six-figure value in Bonanzaville.", "Reality", 1300, 1400, False)
+        construct()
 
+# author: Instructors
+    @weight(1)
+    @number("0.2")
+    def test_show_raises(self):
+        '''Test Show constructor raises error when genre is not one of 6 provided'''
+        def badconstruct(self):
+            return Show("PBS", "Antiques Roadshow", "24-2", "A rare Magic: The Gathering card collection stuns appraisers with a six-figure value in Bonanzaville.", "Human Interest", 1300, 1400, False)
+        self.assertRaises(ValueError, badconstruct,
+          "Show constructor must raise error when genre is not in provided list")
+
+
+# Your tests go here
 
 
 if __name__ == "__main__":
